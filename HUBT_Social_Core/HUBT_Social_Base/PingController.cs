e642b8755982df80335137270118fd7c67027bd7
BinhDuonghb <124377696+BinhDuonghb@users.noreply.github.com>
2025-02-15 23:04:07 +0700

    Create PingController.cs

---
+++ HUBT_Social_Core/HUBT_Social_Base/PingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HUBT_Social_Base
+{
+    public class PingController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok(new { message = "Server is alive" });
+        }
+    }
+}
