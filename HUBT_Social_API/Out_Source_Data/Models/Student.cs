4fce0770bd90aa37d6c6fe0e2d09e7b922626866
BinhDuonghb <124377696+BinhDuonghb@users.noreply.github.com>
2025-02-16 16:16:31 +0700

    out_source_API

---
+++ HUBT_Social_API/Out_Source_Data/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Out_Source_Data.Models
+{
+    [Table("SinhVien")] // Map to the existing table
+    public class Student
+    {
+        [Key]
+        [Required]
+        [Column("MASV")]
+        public string MASV { get; set; } = string.Empty;
+        [Required]
+        [Column("Hoten")]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [Column("NgaySinh")]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        [Column("GioiTinh")]
+        public string Gender { get; set; } = string.Empty;
+        [Required]
+        [Column("TenLop")]
+        public string ClassName { get; set; } = string.Empty;
+    }
+}
