b4352709ee1e89431fcd79afa24c7caaab379f92
BinhDuonghb <124377696+BinhDuonghb@users.noreply.github.com>
2025-02-27 21:49:36 +0700

    gateway update

---
+++ HUBT_Social_API/Gateway_API/JwtConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using HUBT_Social_Core.ASP_Extensions;
+using HUBT_Social_Core.Settings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gateway_API
+{
+    public static class JwtConfiguration
+    {
+        public static IServiceCollection AddJwtConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            JwtSetting? jwtSettings = configuration.GetSection("JwtSettings").Get<JwtSetting>();
+
+            if (jwtSettings != null)
+            {
+                services.ConfigureJwt(jwtSettings);
+                return services;
+            }
+            throw new Exception("Connection fail");
+        }
+    }
+}
--- HUBT_Social_API/Gateway_API/Program.cs
+++ HUBT_Social_API/Gateway_API/Program.cs
@@ -1,18 +1,26 @@
+using HUBT_Social_Core.ASP_Extensions;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace Gateway_API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddJwtConfiguration(builder.Configuration);
+            builder.Configuration.AddJsonFile("router.json", false, true);
+            builder.Configuration.AddJsonFile("ocelot.swagger.json", false, true);
 
+            builder.Services.AddSwaggerForOcelot(builder.Configuration);
+            builder.Services.AddOcelot(builder.Configuration);
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGenService();
 
             var app = builder.Build();
 
@@ -20,17 +28,20 @@ namespace Gateway_API
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerForOcelotUI(options =>
+                {
+                    options.PathToSwaggerGenerator = "/swagger/docs";
+                }).UseOcelot().Wait();
             }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
-
             app.MapControllers();
-
+            await app.UseOcelot();
             app.Run();
         }
     }
 }
+
