dce5ce9b8a3f4342b86251ef4db7e43f62c23a72
BinhDuonghb <124377696+BinhDuonghb@users.noreply.github.com>
2025-03-15 19:32:03 +0700

    udpate gateway chathub

--- HUBT_Social_API/Chat_API/Program.cs
+++ HUBT_Social_API/Chat_API/Program.cs
@@ -1,49 +1,52 @@
 using Chat_API.Configuration;
 using HUBT_Social_Core.ASP_Extensions;
 
-public class Program
+namespace Chat_API
 {
-    private static void InitConfigures(WebApplicationBuilder builder)
+    public class Program
     {
-        builder.Services.AddAuthorization();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGenService();
-        builder.Services.ConfigureLocalization();
-        builder.Services.HttpClientRegisterConfiguration(builder.Configuration);
-    }
-    private static void InitServices(WebApplicationBuilder builder)
-    {
-        builder.Services.AddControllers();
-    }
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+        private static void InitConfigures(WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthorization();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGenService();
+            builder.Services.ConfigureLocalization();
+            builder.Services.HttpClientRegisterConfiguration(builder.Configuration);
+        }
+        private static void InitServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+        }
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        InitConfigures(builder);
-        InitServices(builder);
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            // Add services to the container.
+            InitConfigures(builder);
+            InitServices(builder);
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 
-        var app = builder.Build();
+            var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        //if (app.Environment.IsDevelopment())
-        //{
-        //    app.UseSwagger();
-        //    app.UseSwaggerUI();
-        //}
-        app.UseSwagger();
-        app.UseSwaggerUI();
+            // Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
-        app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
-        app.UseAuthorization();
-        app.UseLocalization();
+            app.UseAuthorization();
+            app.UseLocalization();
 
-        app.MapControllers();
-     
+            app.MapControllers();
 
-        app.Run();
+
+            app.Run();
+        }
     }
 }
