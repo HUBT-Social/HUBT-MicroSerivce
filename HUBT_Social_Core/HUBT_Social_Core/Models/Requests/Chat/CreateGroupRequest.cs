6eadd09f710663f33669e22b21f7f2c677b2d6be
BinhDuonghb <124377696+BinhDuonghb@users.noreply.github.com>
2025-04-27 15:13:27 +0700

    Roomchat create

---
+++ HUBT_Social_Core/HUBT_Social_Core/Models/Requests/Chat/CreateGroupRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HUBT_Social_Core.Models.Requests.Chat
+{
+    public class CreateGroupRequest
+    {
+        [Required]
+        public string GroupName { get; set; } = string.Empty;
+        [Required]
+        public string[] UserNames { get; set; } = [];
+    }
+}
