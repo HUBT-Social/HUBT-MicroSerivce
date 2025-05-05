7e412b09713e9179cbef3d3e7a507de25194cad0
DangVuDev <vuvandang19082004@gmail.com>
2025-05-05 21:10:26 +0700

    done

--- HUBT_Social_API/Gateway_API/Program.cs
+++ HUBT_Social_API/Gateway_API/Program.cs
@@ -79,7 +79,7 @@ namespace Gateway_API
             {
                 options.AddPolicy("AllowReactApp", policy =>
                 {
-                    policy.WithOrigins("https://chatuitest.onrender.com", "http://localhost:3000", "http://localhost:5173")  // Chỉ cho phép origin này
+                    policy.WithOrigins("https://chatuitest.onrender.com", "https://hubt-social-web.onrender.com", "http://localhost:5173")  // Chỉ cho phép origin này
                         .AllowAnyMethod()   // Cho phép bất kỳ phương thức HTTP nào
                         .AllowAnyHeader()   // Cho phép bất kỳ header nào
                         .AllowCredentials(); // Cho phép gửi credentials như cookies, authorization headers
