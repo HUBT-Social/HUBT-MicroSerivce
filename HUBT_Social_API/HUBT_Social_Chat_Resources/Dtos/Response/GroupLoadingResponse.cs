bbb9a5b6d7068bf64dfd4577d5ded5d44d81ab90
DangVuDev <vuvandang19082004@gmail.com>
2025-05-18 14:40:31 +0700

    Update add more attribute response when loading room chat

--- HUBT_Social_API/HUBT_Social_Chat_Resources/Dtos/Response/GroupLoadingResponse.cs
+++ HUBT_Social_API/HUBT_Social_Chat_Resources/Dtos/Response/GroupLoadingResponse.cs
@@ -1,8 +1,11 @@
-﻿namespace HUBT_Social_Chat_Resources.Dtos.Response
+﻿using MongoDB.Bson.Serialization.Serializers;
+
+namespace HUBT_Social_Chat_Resources.Dtos.Response
 {
     public class GroupLoadingResponse : GroupBaseResponse
     {
         public string? LastMessage { get; set; } = string.Empty;
+        public string? LassSender { get; set; } = string.Empty ;
         public string? LastInteractionTime { get; set; } = String.Empty;
     }
 }
