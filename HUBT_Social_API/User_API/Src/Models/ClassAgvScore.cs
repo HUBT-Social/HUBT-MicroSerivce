b1684400f546ceb815548cf8f5dcc2c63df94d66
BinhDuonghb <124377696+BinhDuonghb@users.noreply.github.com>
2025-06-11 20:28:36 +0700

    New API Student class, class Score, get score

---
+++ HUBT_Social_API/User_API/Src/Models/ClassAgvScore.cs
@@ -0,0 +1,40 @@
+﻿namespace User_API.Src.Models
+{
+    public class ClassAgvScore
+    {
+        public int Excellent { get; set; } = 0;
+        public int Good { get; set; } = 0;
+        public int Fair { get; set; } = 0;
+        public int Average { get; set; } = 0;
+        public int BelowAverage { get; set; } = 0;
+        public int Fail { get; set; } = 0;
+
+        public void AddScore(double score)
+        {
+            if (score >= 8.5)
+            {
+                Excellent++;
+            }
+            else if (score >= 7.0)
+            {
+                Good++;
+            }
+            else if (score >= 6.5)
+            {
+                Fair++;
+            }
+            else if (score >= 5.0)
+            {
+                Average++;
+            }
+            else if (score >= 4.0)
+            {
+                BelowAverage++;
+            }
+            else
+            {
+                Fail++;
+            }
+        }
+    }
+}
---
+++ HUBT_Social_API/User_API/Src/Models/StudentScoreOutput.cs
@@ -0,0 +1,34 @@
+﻿using HUBT_Social_Core.Models.OutSourceDataDTO;
+
+namespace User_API.Src.Models
+{
+    public class StudentScoreOutput(ScoreDTO scoreDTO)
+    {
+
+        public string Subject { get => scoreDTO.TenMonHoc; }
+        public double Score10 { get => scoreDTO.Diem;} 
+        public double Score4 => scoreDTO.Diem switch
+        {
+            < 4.0 => 0.0f,
+            < 4.7 => 1.0f,
+            < 5.4 => 1.5f,
+            < 6.2 => 2.0f,
+            < 6.9 => 2.5f,
+            < 7.7 => 3.0f,
+            < 8.5 => 3.5f,
+            _ => 4.0f
+        };
+
+        public string Grade => scoreDTO.Diem switch
+        {
+            < 4 => "F",
+            < 4.7 => "D",
+            < 5.4 => "D+",
+            < 6.2 => "C",
+            < 6.9 => "C+",
+            < 7.7 => "B",
+            < 8.5 => "B+",
+            _ => "A",
+        };
+    }
+}
